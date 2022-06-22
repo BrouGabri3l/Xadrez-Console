f8ea12c30db0e87c2076c848638d435abab536c4
BrouGabri3l <gabriel-rodriguesbs@hotmail.com>
2022-06-22 10:49:37 -0300

    Criada class Posição

---
+++ Xadrez-Console/Entities/Tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}";
+        }
+    }
+}
--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
+using Tabuleiro;
 namespace Xadrez_Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine(P);
         }
     }
 }
