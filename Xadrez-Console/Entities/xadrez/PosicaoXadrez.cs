0d8ebb109c5d83e74616bbcdfb9d389509c954ea
BrouGabri3l <gabriel-rodriguesbs@hotmail.com>
2022-06-22 13:23:32 -0300

    Criando PosicaoXadrez

---
+++ Xadrez-Console/Entities/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return $"{Coluna},{Linha}";
+        }
+    }
+}
--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -7,17 +7,8 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try { 
-            Tabuleiro tab = new Tabuleiro(8,8);
-            
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-            Tela.imprimirTabuleiro(tab);
-            }catch(TabuleiroException e){
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos.ToPosicao());
         }
     }
 }
