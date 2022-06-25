2b879ca0821b5657aab80cadc771760342757fdc
BrouGabri3l <gabriel-rodriguesbs@hotmail.com>
2022-06-25 15:47:45 -0300

    Corrigindo nome do metodo

--- Xadrez-Console/Entities/tabuleiro/Peca.cs
+++ Xadrez-Console/Entities/tabuleiro/Peca.cs
@@ -34,7 +34,7 @@ namespace tabuleiro
             }
             return false;
         }
-        public bool PodeMoverPara(Posicao pos)
+        public bool MovimentoPossivel(Posicao pos)
         {
             return MovimentosPossiveis()[pos.Linha, pos.Coluna];
         }
--- Xadrez-Console/Entities/xadrez/PartidaXadrez.cs
+++ Xadrez-Console/Entities/xadrez/PartidaXadrez.cs
@@ -106,7 +106,7 @@ namespace xadrez
         }
         public void ValidarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if (!Tab.Peca(origem).PodeMoverPara(destino))
+            if (!Tab.Peca(origem).MovimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posição de destino inválida");
             }
