11864c0f092ba16ad4428bac4c0224ea152dea15
BrouGabri3l <gabriel-rodriguesbs@hotmail.com>
2022-06-25 23:23:46 -0300

    Implementando jogada Promoção

--- Xadrez-Console/Entities/xadrez/PartidaXadrez.cs
+++ Xadrez-Console/Entities/xadrez/PartidaXadrez.cs
@@ -86,6 +86,19 @@ namespace xadrez
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque");
             }
+            Peca p = Tab.Peca(destino);
+            //#jogadaespecial promoção
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -103,7 +116,7 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-            Peca p = Tab.Peca(destino);
+            
             //#jogadaespecial en passant
             if (p is Peao && destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2)
             {
