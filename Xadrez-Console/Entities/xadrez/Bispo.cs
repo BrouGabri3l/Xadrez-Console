77137838709fcd114888699f413821045016b731
BrouGabri3l <gabriel-rodriguesbs@hotmail.com>
2022-06-25 21:14:16 -0300

    Consertando bispo

--- Xadrez-Console/Entities/xadrez/Bispo.cs
+++ Xadrez-Console/Entities/xadrez/Bispo.cs
@@ -24,10 +24,10 @@ namespace xadrez
                 {
                     break;
                 }
-                pos.Linha++;
+                pos.DefinirValores(pos.Linha - 1, pos.Coluna + 1);
             }
             //nororeste
-            pos.DefinirValores(Posicao.Linha -1, Posicao.Coluna - 1);
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
             while (Tab.PosicaoValida(pos) && PodeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
@@ -35,10 +35,10 @@ namespace xadrez
                 {
                     break;
                 }
-                pos.Linha++;
+                pos.DefinirValores(pos.Linha - 1, pos.Coluna - 1);
             }
             //sudeste
-            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna +1);
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
             while (Tab.PosicaoValida(pos) && PodeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
@@ -46,11 +46,11 @@ namespace xadrez
                 {
                     break;
                 }
-                pos.Linha++;
+                pos.DefinirValores(pos.Linha + 1, pos.Coluna + 1);
             }
 
             //sudoeste
-            pos.DefinirValores(Posicao.Linha+1, Posicao.Coluna - 1);
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
             while (Tab.PosicaoValida(pos) && PodeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
@@ -58,7 +58,7 @@ namespace xadrez
                 {
                     break;
                 }
-                pos.Coluna--;
+                pos.DefinirValores(pos.Linha + 1, pos.Coluna - 1);
             }
             return mat;
 
